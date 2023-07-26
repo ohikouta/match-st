d5a9099c724da3a51581221750caab9c99d431b9
ohikouta <koutaft273456@gmail.com>
2023-07-27 00:58:39 +0900

    DBレコード追加、indexページ作成と、viewにDB情報を反映させた

---
+++ resources/views/bases/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Student Information</title>
+        <!-- Fonts -->
+        <link href="https://googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Student Information</h1>
+        <div class='self'>
+            @foreach ($bases as $base)
+                <h2>{{ $base->name }}</h2>
+                <p>{{ $base->univ }}</p>
+            @endforeach
+        </div>
+    </body>
+</html>
