6d214f98f9b5e13fbcf4455253bfa12efdd74fb1
ohikouta <koutaft273456@gmail.com>
2023-08-05 02:40:05 +0900

    univsテーブルの作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,44 +14,6 @@ class UserController extends Controller
         return view('users.create');
     }
     
-    /*
-    public function store(Request $request, Base $base)
-    {
-        dd($request->all());
-
-        // バリデーションが必要な場合はここでバリデーションを行う
-        
-        // データベースのトランザクションを開始
-        DB::beginTransaction();
-        try {
-            // フォームのデータをモデルに保存
-            $user = new Base();
-            $user->name = $request->input('name');
-            $user->univ = $request->input('univ');
-            // 他のフォームデータも必要に応じてセットする
-            
-            $user->save(); // データベースに保存
-            
-            // トランザクションのコミット
-            DB::commit();
-            
-            // データの保存後、リダイレクトなどの適切な処理を行う
-            return redirect()->route('users.index')->with('success', 'ユーザーが作成されました');
-            
-        } catch (\Exception $e) {
-            //  トランザクションのロールバック
-            DB::rollback();
-            
-            // エラーログなどにエラー情報を記録することも可能
-            
-            // エラー処理
-            return redirect()->back()->with('error', 'ユーザーの作成に失敗しました。');
-        }
-        
-        // フォームからのデータをモデルに保存
-    }
-    */
-    
     public function show(Base $base)
 	{
 		return view('users/show')->with(['base' => $base]);
