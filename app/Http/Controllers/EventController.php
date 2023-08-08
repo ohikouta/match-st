39e20ad084d378ebe0c1cde18b5ba7107e69c7db
ohikouta <koutaft273456@gmail.com>
2023-08-09 03:21:56 +0900

    小変更

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Event;
 
 class EventController extends Controller
 {
@@ -15,17 +14,5 @@ class EventController extends Controller
     public function show()
     {
         return view('events.plan');
-    }
-    
-    public function showResult(Event $event)
-    {
-        return view('events.show')->with(['event' => $event]);
-    }
-    
-    public function store(Request $request, Event $event)
-    {
-        $input = $request['event'];
-        $event->fill($input)->save();
-        return redirect('/events/' . $event->id);
     }
 }
