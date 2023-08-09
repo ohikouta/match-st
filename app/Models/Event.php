be81537fba4980140e2bb6607443e9e1b5ea0f48
ohikouta <koutaft273456@gmail.com>
2023-08-09 17:15:05 +0900

    イベント一覧ページ作成

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 
 class Event extends Model
 {
@@ -13,4 +14,9 @@ class Event extends Model
         'name',
         'summary',
         ];
+        
+    public function getPaginateByLimit(int $limit_count=5)
+    {
+        return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
