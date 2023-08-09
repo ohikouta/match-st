d091b046917a49c6cf0251511beca19de7dc657e
ohikouta <koutaft273456@gmail.com>
2023-08-09 16:27:47 +0900

    イベント一覧ページ作成

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
@@ -15,4 +16,11 @@ class EventController extends Controller
     {
         return view('events.plan');
     }
+    
+    public function look(Event $event)
+    {
+        return view('events.look')->with(['events' => $event->getPaginateBylimit(5)]);
+    }
+    
+
 }
---
+++ resources/views/events/look.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Student Information</title>
+        <!-- Fonts -->
+        <link href="https://googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    </head>
+    <body>
+        <h1>イベント掲示板</h1>
+        <div class='self'>
+            @foreach ($events as $event)
+                <h2>{{ $event->name }}</h2>
+            @endforeach
+        </div>
+        <div class='paginate'>{{ $events->links('pagination::bootstrap-4') }}</div>
+    </body>
+</html>
