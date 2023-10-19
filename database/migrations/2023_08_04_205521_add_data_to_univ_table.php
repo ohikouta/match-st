0f9a5fa4467484a5e6851f0444f5871c29853fb0
ohikouta <koutaft273456@gmail.com>
2023-10-19 20:48:24 +0900

    自動挿入に変更-migration

--- database/migrations/2023_08_04_205521_add_data_to_univ_table.php
+++ database/migrations/2023_08_04_205521_add_data_to_univ_table.php
@@ -8,19 +8,13 @@ class AddNewDataToTable extends Migration
 {
     public function up()
     {
-        // まずunivsテーブルが存在するか確認
-        if (Schema::hasTable('univs')) {
-            // テーブルが存在する場合、データを挿入
-            DB::table('univs')->insert([
-                'univ_name' => 'ChibaUniversityofCommerce',
-                'locate' => 'Chiba',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        } else {
-            // テーブルが存在しない場合、エラーメッセージを表示
-            $this->command->error('univs table does not exist. Please run the migration to create it.');
-        }
+        // データを挿入
+        DB::table('univs')->insert([
+            'univ_name' => 'ChibaUniversityofCommerce',
+            'locate' => 'Chiba',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     public function down()
