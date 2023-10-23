96efcb3d13a2993212629e6837974dfdcaa17e78
ohikouta <koutaft273456@gmail.com>
2023-10-23 10:50:18 +0900

    リクエスト処理後の繊維画面実装

--- resources/views/events/request_result.blade.php
+++ resources/views/events/request_result.blade.php
@@ -12,9 +12,31 @@
         <script src="{{ asset('/js/map.js') }}"></script>
     </head>
     <body>
+        
+        <main class="flex justify-center">
+            <div>
+            </div>
+        </main>
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Student Information</title>
+        <!-- Fonts -->
+        <link href="https://googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="https://kit.fontawesome.com/48447305da.js" crossorigin="anonymous"></script>
+        @vite(['resources/css/app.css'])
+        <script src="https://code.jquery.com/jquery.js"></script> 
+        <script src="{{ asset('/js/map.js') }}"></script>
+    </head>
+    <body>
         <header>
             <div class="bg-blue-500 text-white p-4">
-                <h1 class="text-4xl font-bold">IEEE ～仲間をつくる～</h1>
+                <a href="{{ route('users.index') }}" class="block text-4xl font-bold">IEEE ～仲間をつくる～</a>
                 <!-- /users/create へのリンクを絶対URLで生成 -->
             </div>
             <!-- ナビゲーションセクション -->
@@ -33,11 +55,21 @@
             </div>
         </header>
         <main class="flex justify-center">
-            <div>
-                <p>下記のイベントに応募しました</p>
-                <p>{{ $event->name }}</p>
-                <p>{{ $event->summary }}</p>
-                <p>{{ $event->date }}</p>
+            <!-- map表示のためのアドレス指定 -->
+            <div class="hidden">
+                <span id="eventAddress">{{ $event->address }}</span>
+            </div>
+            <div class="w-3/4 my-10 p-5 flex flex-col items-center border border-solid rounded-lg shadow-md">
+                <div class="flex-col ">
+                    <p>下記のイベントに応募しました</p>
+                    <h2 class="border-l-4 border-blue-500 pl-2 text-xl font-bold mb-2">タイトル</h2>
+                    <p class="font-bold mb-4 pl-4">{{ $event->name }}</p>
+                    <h2 class="border-l-4 border-blue-500 pl-2 text-xl font-bold mb-2">概要</h2>
+                    <p class="font-bold mb-4 pl-4">{{ $event->summary }}</p>
+                    <h2 class="border-l-4 border-blue-500 pl-2 text-xl font-bold mb-2">開催日時</h2>
+                    <p class="font-bold mb-4 pl-4">{{ $event->date }}</p>
+                </div>
+                <a href="{{ route('users.index') }}" class="font-bold text-white bg-green-500 px-10 py-2 rounded-md hover:bg-green-600">ホームに戻る</a>
             </div>
             
         </main>
